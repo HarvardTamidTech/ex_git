e7d31296e84121ca80367a74e503e309c736904b
jpearl8 <31544850+jpearl8@users.noreply.github.com>
2019-01-08 15:11:52 -0500

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,13 @@
+print('git excersize')
+
+#make a function that takes in 2 ints, compares them, prints out smaller
+def cmp_less(num1, num2):
+	print('to do')
+
+
+#make a function that takes in a string and prints out first item
+def print_first(str1):
+	print('to do')
+
+cmp_less(1, 2)
+print_first("test")
