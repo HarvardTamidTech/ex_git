33ae25e5cffc45712e42da7a54caaabc4e01c63b
Jerry Yang <jerryyang@dhcp-10-250-191-220.harvard.edu>
2019-02-26 19:54:59 -0500

    test

--- ex.py
+++ ex.py
@@ -2,12 +2,12 @@ print('git excersize')
 
 #make a function that takes in 2 ints, compares them, prints out smaller
 def cmp_less(num1, num2):
-	print('to do')
+	print(min(num1, num2))
 
 
 #make a function that takes in a string and prints out first item
 def print_first(str1):
-	print('to do')
+	print(str1[0])
 
 cmp_less(1, 2)
 print_first("test")
